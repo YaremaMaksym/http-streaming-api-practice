3bb834a0f422809020757448990fa0e0f9c535ea
Maksym Yarema <macsym2@gmail.com>
2024-01-30 12:34:21 +0200

    init project

---
+++ src/test/java/org/yaremax/httpstreamingapipractice/HttpStreamingApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yaremax.httpstreamingapipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpStreamingApiPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
